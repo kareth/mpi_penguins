d3e3914447091fc52770645c104ba11028628a0b
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2014-06-16 18:16:10 +0200

    configuration

---
+++ src/configuration.h
@@ -0,0 +1,13 @@
+#ifndef PENGUINS_CONFIGURATION_H_
+#define PENGUINS_CONFIGURATION_H_
+
+namespace penguins {
+
+namespace Configuration {
+  const int MaxShips = 15;
+  const int MaxPorts = 10;
+}
+
+}
+
+#endif
